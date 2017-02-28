dc1d2191c37eca220f512cde4391a628770382b5
sunflower-git <nati.mamaiashvili@gmail.com>
2017-02-28 18:29:28 +0400

    Visual BubbleSortAlgorithm

---
+++ MyBubbleSorting/MyBubbleSorting1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace MyBubbleSorting
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(50, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 35);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "10";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(50, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "10";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(50, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 35);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "10";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(50, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 35);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "10";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(50, 252);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 35);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "10";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(50, 287);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(49, 35);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "10";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(250, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(30, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Slow";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(250, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(27, 13);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Fast";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(2, 74);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(153, 73);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "[     ]";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(253, 95);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 210);
+            this.trackBar1.TabIndex = 10;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(50, 362);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 41);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(223, 362);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 41);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Sort";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(206, 198);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(13, 13);
+            this.label11.TabIndex = 13;
+            this.label11.Text = "1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 424);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label10);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ MyBubbleSorting/MyBubbleSorting1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Natia Mamaiashvili Group 1
+namespace MyBubbleSorting
+{
+    //Inicialization of arrays and giving them random numbers;
+    public partial class Form1 : Form
+    {
+        Random gen = new Random();
+        Label[] arr = new Label[7];
+        int[] y = { 77, 112, 147, 182, 217, 252, 287 };
+        int counter1, counter2, check, swapIndex = 0;
+        int changer;
+        string status = "think";
+        public Form1()
+        {
+            InitializeComponent();
+            arr[0] = label1;
+            arr[1] = label2;
+            arr[2] = label3;
+            arr[3] = label4;
+            arr[4] = label5;
+            arr[5] = label6;
+            arr[6] = label7;
+
+
+            label1.Top = y[0];
+            label2.Top = y[1];
+            label3.Top = y[2];
+            label4.Top = y[3];
+            label5.Top = y[4];
+            label6.Top = y[5];
+            label7.Top = y[6];
+
+            label1.Text = gen.Next(1, 100).ToString();
+            label2.Text = gen.Next(1, 100).ToString();
+            label3.Text = gen.Next(1, 100).ToString();
+            label4.Text = gen.Next(1, 100).ToString();
+            label5.Text = gen.Next(1, 100).ToString();
+            label6.Text = gen.Next(1, 100).ToString();
+            label7.Text = gen.Next(1, 100).ToString();
+
+        }
+
+        // Reset Button: aranging labels in previous order and assignig them random numbers;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            arr[0] = label1;
+            arr[1] = label2;
+            arr[2] = label3;
+            arr[3] = label4;
+            arr[4] = label5;
+            arr[5] = label6;
+            arr[6] = label7;
+
+            label1.Top = y[0];
+            label2.Top = y[1];
+            label3.Top = y[2];
+            label4.Top = y[3];
+            label5.Top = y[4];
+            label6.Top = y[5];
+            label7.Top = y[6];
+
+
+            label1.Text = gen.Next(1, 100).ToString();
+            label2.Text = gen.Next(1, 100).ToString();
+            label3.Text = gen.Next(1, 100).ToString();
+            label4.Text = gen.Next(1, 100).ToString();
+            label5.Text = gen.Next(1, 100).ToString();
+            label6.Text = gen.Next(1, 100).ToString();
+            label7.Text = gen.Next(1, 100).ToString();
+            label10.Location = new System.Drawing.Point(2, 74);
+            button2.Enabled = true;
+
+            //Changing Color To Black;
+            for (int i = 0; i < 7; i++)
+                arr[i].ForeColor = Color.Black;
+            changer = 5;
+            timer1.Stop();
+        }
+
+        // Sort Button : timer Starts and thus the timer tick starts and bubble sort algorithm;
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            changer = 5;
+            counter1 = 0;
+            counter2 = 0;
+            swapIndex = 0;
+            check = 5;
+            label10.Top = 74;
+            timer1.Start();
+            button2.Enabled = false;
+            button1.Enabled = false;
+        }
+
+        // Bubble Sort Algorithm with three status: move, think and swap step;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (status == "swap")
+            {
+                if (counter1 > 34)
+                {
+                    timer1.Stop();
+                    Label temp = arr[swapIndex];
+                    arr[swapIndex] = arr[swapIndex + 1];
+                    arr[swapIndex + 1] = temp;
+                    counter1 = 0;
+                    status = "move";
+                    timer1.Start();
+                    return;
+
+                }
+
+                counter1++;
+                arr[swapIndex].Top += 1;
+                arr[swapIndex + 1].Top -= 1;
+
+            }
+
+            else if (status == "move")
+            {
+                if (changer < 0)
+                {
+                    arr[swapIndex].ForeColor = Color.Green;
+                    
+                    button1.Enabled = true;
+                    return;
+                }
+                if (swapIndex ==changer)
+                {
+                    if (changer != -2)
+                    {
+                        arr[changer +1].ForeColor = Color.Green;
+                        check -= 34;
+                        label10.Top = 74;
+                    }
+                    changer--;
+                    swapIndex = 0;
+                    label10.Top = 74;
+                    status = "think";
+                    
+                }
+                else
+                {
+
+                    if (counter2 > 34)
+                    {
+
+                        swapIndex++;
+                        counter2 = 0;
+                        status = "think";
+                        timer1.Start();
+                        return;
+
+                    }
+                    counter2++;
+                    label10.Top += 1;
+                }
+
+            }
+            else if (status == "think")
+            {
+                timer1.Stop();
+                if (Convert.ToInt32(arr[swapIndex].Text) > Convert.ToInt32(arr[swapIndex + 1].Text))
+                {
+                    status = "swap";
+                }
+                else
+                    status = "move";
+
+
+                timer1.Start();
+            }
+        }
+
+        // Changing track bar values;
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            label11.Text = trackBar1.Value.ToString();
+            timer1.Interval = trackBar1.Value / 2 + 1;
+        }
+    }
+}
+
